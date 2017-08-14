c570c5002ade58793d56dfdf1330835bfe03cff7
Melvin <melvin0719@hotmail.com>
2017-08-14 09:21:38 +0800

    Change sqlite to pgsql

--- cheerbot_db.py
+++ cheerbot_db.py
@@ -1,142 +1,249 @@
-import sqlite3
+import os
+import psycopg2
 import datetime_util
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
 class Cheerbot_DB:
-	def __init__(self, db_name = 'cheerbot.sqlite'):
-		self.db_name = db_name
-		self.conn = sqlite3.connect(db_name, check_same_thread = False)
+    def __init__(self):
+        self.conn = psycopg2.connect(DATABASE_URL)
+        print('connected')
 
-	def drop_tables(self):
-		training_table = 'DROP TABLE IF EXISTS trainings'
-		user_table = 'DROP TABLE IF EXISTS users'
-		attendance_table = 'DROP TABLE IF EXISTS attendances'
-		self.conn.execute(attendance_table)
-		self.conn.execute(training_table)
-		self.conn.execute(user_table)
-		self.conn.commit()
+    def drop_tables(self):
+        cur = self.conn.cursor()
+        cur.execute('DROP TABLE IF EXISTS trainings')
+        cur.execute('DROP TABLE IF EXISTS users')
+        cur.execute('DROP TABLE IF EXISTS attendances')
+        self.conn.commit()
 
-	def setup(self):
-		training_table = 'CREATE TABLE IF NOT EXISTS trainings (' \
-			+ 'training_id integer, ' \
-			+ 'venue text, ' \
-			+ 'start_datetime datetime, ' \
-			+ 'end_datetime datetime, ' \
-			+ 'training_type text)'
-		user_table = 'CREATE TABLE IF NOT EXISTS users (' \
-			+ 'user_id integer, ' \
-			+ 'user_name text, ' \
-			+ 'name text, ' \
-			+ 'admin boolean)'
-		attendance_table = 'CREATE TABLE IF NOT EXISTS attendances (' \
-			+ 'training_id integer, ' \
-			+ 'user_id integer, ' \
-			+ 'coming boolean, ' \
-			+ 'reason text)'
-		self.conn.execute(training_table)
-		self.conn.execute(user_table)
-		self.conn.execute(attendance_table)
-		self.conn.commit()
+    def setup(self):
+        cur = self.conn.cursor()
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS trainings (
+                training_id integer,
+                venue text,
+                start_datetime datetime,
+                end_datetime datetime,
+                training_type text
+            );
+            """
+        )
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                user_id integer,
+                user_name text,
+                name text,
+                admin boolean
+            );
+            """
+        )
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS attendances (
+                training_id integer,
+                user_id integer,
+                coming boolean,
+                reason text
+            );
+            """
+        )
+        self.conn.commit()
+        print("setup")
 
-	def get_table_size(self, table):
-		statement = 'SELECT COUNT(*) FROM ' + table
-		return self.conn.execute(statement).fetchone()[0]
+    def get_table_size(self, table):
+        sql = """
+              SELECT COUNT(*)
+              FROM %(table)s;
+              """
+        data = {'table': table}
+        cur = self.conn.cursor()
+        return cur.execute(statement).fetchone()[0]
 
-	def add_training(self, venue, start_datetime, end_datetime, training_type):
-		training_id = self.get_table_size('trainings') + 1
-		statement = 'INSERT INTO trainings (training_id, venue, start_datetime, end_datetime, training_type) ' \
-			+ ' VALUES (?, ?, ?, ?, ?)'
-		args = (training_id, venue, start_datetime, end_datetime, training_type)
-		self.conn.execute(statement, args)
-		self.conn.commit()
+    def add_training(self, venue, start_datetime, end_datetime, training_type):
+        training_id = self.get_table_size('trainings') + 1
+        sql = """
+              INSERT INTO trainings (training_id, venue, start_datetime, end_datetime, training_type)
+              VALUES (%(training_id)s, %(venue)s, %(start_datetime)s, %(end_datetime)s, %(training_type)s);
+              """
+        data = {'training_id': training_id,
+                'venue': venue,
+                'start_datetime': start_datetime,
+                'end_datetime': end_datetime,
+                'training_type': training_type}
+        cur = self.conn.cursor()
+        cur.execute(sql, data)
+        self.conn.commit()
 
-	def find_training(self, training_id):
-		statement = 'SELECT * FROM trainings WHERE training_id = (?) LIMIT 1'
-		args = (training_id,)
-		return [x for x in self.conn.execute(statement, args)]
+    def find_training(self, training_id):
+        sql = """
+              SELECT * 
+              FROM trainings 
+              WHERE training_id = %(training_id)s
+              LIMIT 1;
+              """
+        data = {'training_id': training_id}
+        cur = self.conn.cursor()
+        return [x for x in cur.execute(sql, data).fetchall()]
 
-	def get_upcoming_trainings(self):
-		datetime_now = datetime_util.get_now()
-		statement = "SELECT * FROM trainings WHERE (?) < start_datetime ORDER BY start_datetime"
-		args = (datetime_now,)
-		return [x for x in self.conn.execute(statement, args)]
+    def get_upcoming_trainings(self):
+        datetime_now = datetime_util.get_now()
+        sql = """
+              SELECT * 
+              FROM trainings 
+              WHERE %(datetime_now)s < start_datetime 
+              ORDER BY start_datetime;
+              """
+        data = {'datetime_now' : datetime_now}
+        cur = self.conn.cursor()
+        return [x for x in cur.execute(sql, data).fetchall()]
 
-	def get_current_and_upcoming_trainings(self):
-		datetime_now = datetime_util.get_now()
-		statement = "SELECT * FROM trainings WHERE (?) < end_datetime ORDER BY start_datetime"
-		args = (datetime_now,)
-		return [x for x in self.conn.execute(statement, args)]
+    def get_current_and_upcoming_trainings(self):
+        datetime_now = datetime_util.get_now()
+        sql = """
+              SELECT * 
+              FROM trainings 
+              WHERE %(datetime_now)s < end_datetime 
+              ORDER BY start_datetime;
+              """
+        data = {'datetime_now' : datetime_now}
+        cur = self.conn.cursor()
+        return [x for x in cur.execute(sql, data).fetchall()]
 
-	def find_user(self, user_id):
-		statement = 'SELECT * FROM users WHERE user_id = (?) LIMIT 1'
-		args = (user_id,)
-		return [x for x in self.conn.execute(statement, args)]
+    def find_user(self, user_id):
+        sql = """
+              SELECT *
+              FROM users
+              WHERE user_id = %(user_id)s
+              LIMIT 1;
+              """
+        statement = 'SELECT * FROM users WHERE user_id = (?) LIMIT 1'
+        data = {'user_id': user_id}
+        return [x for x in cur.execute(sql, data).fetchall()]
 
-	def update_user_info(self, user_id, user_name, name):
-		statement = 'UPDATE users SET user_name = (?), name = (?) WHERE user_id = (?)'
-		args = (user_name, name, user_id)
-		self.conn.execute(statement, args)
-		self.conn.commit()
+    def update_user_info(self, user_id, user_name, name):
+        sql = """
+              UPDATE users
+              SET user_name = %(user_name)s, name = %(name)s
+              WHERE user_id = %(user_id)s;
+              """
+        data = {'user_name': user_name,
+                'user_id': user_id,
+                'name': name}
+        cur = self.conn.cursor()
+        cur.execute(sql, data)
+        self.conn.commit()
 
-	def set_user_access(self, user_id, is_admin):
-		statement = 'UPDATE users SET admin = (?) WHERE user_id = (?)'
-		admin = 1 if is_admin else 0
-		args = (admin, user_id)
-		self.conn.execute(statement, args)
-		self.conn.commit()
+    def set_user_access(self, user_id, is_admin):
+        sql = """
+              UPDATE users
+              SET admin = %(admin)s
+              WHERE user_id = %(user_id)s;
+              """
+        admin = 1 if is_admin else 0
+        args = {'admin': admin,
+                'user_id': user_id}
+        cur = self.conn.cursor()
+        cur.execute(sql, data)
+        self.conn.commit()
 
-	def add_user(self, user_id, user_name, name):
-		statement = 'INSERT INTO users (user_id, user_name, name, admin) ' \
-			+ ' VALUES (?, ?, ?, ?)'
-		args = (user_id, user_name, name, 0)
-		self.conn.execute(statement, args)
-		self.conn.commit()
+    def add_user(self, user_id, user_name, name):
+        sql = """
+              INSERT INTO users (user_id, user_name, name, admin)
+              VALUES (%(user_id)s, %(user_name)s, %(name)s, %(admin)s);
+              """
+        data = {'user_id': user_id,
+                'user_name': user_name,
+                'name': name,
+                'admin': admin}
+        cur = self.conn.cursor()
+        cur.execute(sql, data)
+        self.conn.commit()
 
-	def add_or_update_user(self, user_id, user_name, name):
-		if len(self.find_user(user_id)) == 0:
-			self.add_user(user_id, user_name, name)
-		else:
-			self.update_user_info(user_id, user_name, name)
+    def add_or_update_user(self, user_id, user_name, name):
+        if len(self.find_user(user_id)) == 0:
+            self.add_user(user_id, user_name, name)
+        else:
+            self.update_user_info(user_id, user_name, name)
 
-	def find_attendance(self, training_id, user_id):
-		statement = 'SELECT * FROM attendances WHERE training_id = (?) AND user_id = (?) LIMIT 1'
-		args = (training_id, user_id)
-		return [x for x in self.conn.execute(statement, args)]
+    def find_attendance(self, training_id, user_id):
+        sql = """
+              SELECT *
+              FROM attendances
+              WHERE training_id = %(training_id)s AND user_id = %(user_id)s
+              LIMIT 1
+              """
+        data = {'training_id': training_id,
+                'user_id': user_id}
+        cur = self.conn.cursor()
+        return [x for x in cur.execute(sql, data).fetchall()]
 
-	def get_attendances(self, training_id):
-		statement = 'SELECT * FROM attendances WHERE training_id = (?) ORDER BY coming'
-		args = (training_id,)
-		return [x for x in self.conn.execute(statement, args)]
+    def get_attendances(self, training_id):
+        sql = """
+              SELECT *
+              FROM attendances
+              WHERE training_id = %(training_id)s
+              ORDER BY coming
+              """
+        data = {'training_id': training_id}
+        cur = self.conn.cursor()
+        return [x for x in cur.execute(sql, data).fetchall()]
 
-	def get_no_reply_user_ids(self, training_id):
-		statement = 'SELECT user_id FROM users EXCEPT SELECT user_id FROM attendances WHERE training_id = (?)'
-		args = (training_id,)
-		return [x for x in self.conn.execute(statement, args)]
+    def get_no_reply_user_ids(self, training_id):
+        sql = """
+              SELECT user_id
+              FROM users
+              EXCEPT
+              SELECT user_id
+              FROM attendances
+              WHERE training_id = %(training_id)s
+              """
+        data = {'training_id': training_id}
+        cur = self.conn.cursor()
+        return [x for x in cur.execute(sql, data).fetchall()]
 
-	def add_attendance(self, training_id, user_id, coming):
-		statement = 'INSERT INTO attendances (training_id, user_id, coming) ' \
-			+ ' VALUES (?, ?, ?)'
-		args = (training_id, user_id, coming)
-		self.conn.execute(statement, args)
-		self.conn.commit()
+    def add_attendance(self, training_id, user_id, coming):
+        sql = """
+              INSERT INTO attendances (training_id, user_id, coming)
+              VALUES (%(training_id)s, %(user_id)s, %(coming)s)
+              """
+        data = {'training_id': training_id,
+                'user_id': user_id,
+                'coming': coming}
+        cur = self.conn.cursor()
+        cur.execute(sql, data)
+        self.conn.commit()
 
-	def update_attendance(self, training_id, user_id, coming):
-		statement = 'UPDATE attendances SET coming = (?) WHERE training_id = (?) AND user_id = (?)'
-		args = (coming, training_id, user_id)
-		self.conn.execute(statement, args)
-		self.conn.commit()
+    def update_attendance(self, training_id, user_id, coming):
+        sql = """
+              UPDATE attendances
+              SET coming = %(coming)s
+              WHERE training_id = %(training_id)s AND user_id = %(user_id)s
+              """
+        data = {'training_id': training_id,
+                'user_id': user_id,
+                'coming': coming}
+        cur = self.conn.cursor()
+        cur.execute(sql, data)
+        self.conn.commit()
 
-	def add_or_update_attendance(self, msg):
-		can_attend = 1 if msg['data'].startswith('/can_go') else 0
-		training_id = msg['data'].split(' ')[1]
-		# reason = ' '.join(msg['data'].split(' ')[2:])
-		# Handle user
-		chat = msg['message']['chat']
-		user_id = chat['id']
-		user_name = chat['username'] if 'username' in chat else ''
-		name = chat['first_name'] if 'first_name' in chat else ''
-		self.add_or_update_user(user_id, user_name, name)
-		# Handle attendance
-		if len(self.find_attendance(training_id, user_id)) == 0:
-			self.add_attendance(training_id, user_id, can_attend)
-		else:
-			self.update_attendance(training_id, user_id, can_attend)
+    def add_or_update_attendance(self, msg):
+        can_attend = 1 if msg['data'].startswith('/can_go') else 0
+        training_id = msg['data'].split(' ')[1]
+        # reason = ' '.join(msg['data'].split(' ')[2:])
+        # Handle user
+        chat = msg['message']['chat']
+        user_id = chat['id']
+        user_name = chat['username'] if 'username' in chat else ''
+        name = chat['first_name'] if 'first_name' in chat else ''
+        self.add_or_update_user(user_id, user_name, name)
+        # Handle attendance
+        if len(self.find_attendance(training_id, user_id)) == 0:
+            self.add_attendance(training_id, user_id, can_attend)
+        else:
+            self.update_attendance(training_id, user_id, can_attend)
+
+db = Cheerbot_DB()
+db.setup()
+print(db.get_table_size('trainings'))
